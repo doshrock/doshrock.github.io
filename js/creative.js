e5ab12e778aef1df95b420b82ad8f614430692a9
Ray <sparkiss@gmail.com>
2025-11-18 14:40:25 -0500

    menu updated

--- js/creative.js
+++ js/creative.js
@@ -18,8 +18,8 @@
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
+        target: '#mainNav',
+        offset: 100
     })
 
     // Closes the Responsive Menu on Menu Item Click
